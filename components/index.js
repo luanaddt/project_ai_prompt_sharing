2fd7cef08e0b6d1ddc1bec15e5dfdf9bfccd44b1
LDS <luanddt.dev@gmail.com>
2024-01-07 18:15:20 +0700

    update feed

--- components/index.js
+++ components/index.js
@@ -2,10 +2,14 @@ import Feed from "./Feed";
 import Navbar from "./Navbar";
 import Provider from "./Provider";
 import Form from "./Form";
+import PromptCardList from "./PromptCardList"
+import PromptCard from "./PromptCard";
 
 export {
     Feed,
     Navbar,
     Provider,
-    Form
+    Form,
+    PromptCardList,
+    PromptCard
 }
