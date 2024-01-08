bb69e77043af67fa676b5d1e80405de5e3cda319
LDS <luanddt.dev@gmail.com>
2024-01-08 20:06:31 +0700

    update nextjs api routes

--- app/profile/page.js
+++ app/profile/page.js
@@ -10,6 +10,8 @@ const MyProfile = () => {
 
     const { data: session } = useSession()
 
+    const router = useRouter()
+
     useEffect(() => {
         const fetchPosts = async () => {
             const response = await fetch(`/api/users/${session?.user.id}/posts`)
@@ -22,12 +24,26 @@ const MyProfile = () => {
         if(session?.user.id) fetchPosts()
     }, [])
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
 
+        if(hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: "DELETE"
+                })
+
+                const filteredPosts = posts.filter((p) => p._id !== post._id)
+
+                setPosts(filteredPosts)
+            } catch (error) {
+                console.log(error);
+            }
+        }
     }
 
     return (
